a1507b3578235436edace9731ce9f99f72c49584
Rho JungHun <noelfania@naver.com>
2024-02-18 22:31:21 +0900

    20240218 RHO

---
+++ dev/test.ts
@@ -0,0 +1,11 @@
+class V {
+    x: number;
+    y: number;
+
+    constructor(x:number,y:number){
+        this.x = x;
+        this.y=y;
+    }
+}
+const vvv= new V("13",14)
+vvv.x
---
+++ dist/test.js
@@ -0,0 +1,9 @@
+"use strict";
+class V {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+const vvv = new V("13", 14);
+vvv.x;
